a0308fe1203cf65d54578e25b553701a6291d1b8
MaZhehua <lz_mzh@aliyun.com>
2022-04-02 13:35:45 +0800

    feat: add some libraries & gdb

--- run_development.sh
+++ run_development.sh
@@ -4,11 +4,14 @@ container_name="development_env"
 
 docker rm -f $container_name
 
-docker run -d -P --name=$container_name \
+docker run -d --name=$container_name -P \
+    --security-opt seccomp=unconfined \
     -v /:/host \
     -v /etc/localtime:/etc/localtime \
     -v /etc/timezone:/etc/timezone \
-    dockerhub.deepglint.com/atlas/developmentkit_advanced:v0.3-x86_64
+    -v /usr/bin/docker:/usr/bin/docker \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    dockerhub.deepglint.com/atlas/developmentkit_advanced:v0.4-x86_64
 
 docker port $container_name
 
