6cbc1a3392b3a1fef35b23d15057fd1caef081c0
MaZhehua <lz_mzh@aliyun.com>
2022-04-08 15:17:43 +0800

    fix: replace base image tag in advanced/Dockerfile

--- build_images.sh
+++ build_images.sh
@@ -34,4 +34,6 @@ function build_image() {
 }
 
 build_image ${base_repo}:${base_tag}
+
+sed -i "1s/.*/FROM ${base_repo}:${base_tag}/" $(pwd)/advanced/Dockerfile
 build_image ${advanced_repo}:${advanced_tag}
